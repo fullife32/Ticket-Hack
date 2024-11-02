365c390c80bd9104f4b681ca3a9442a41e78352e
cardiox12 <com.bellavi@tutanota.com>
2024-11-02 11:24:17 +0100

    WIP

--- src/back/ticket_sdk/ticket.py
+++ src/back/ticket_sdk/ticket.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import List
 from pydantic import BaseModel, Field, confloat, conint, constr, validator
 
+from ticket_sdk.fake_ticket import FakeTicket
 
-class Ticket(BaseModel):
+
+class TicketEmitterDto(BaseModel):
     ticket_numbers: conint(gt=0)
     event_name: constr(min_length=3, max_length=100)
     unary_price: confloat(gt=0.1)
@@ -14,3 +17,25 @@ class Ticket(BaseModel):
         if v <= datetime.now():
             raise ValueError("Date must be set to future")
         return v
+
+class TicketsManager:
+    def __init__(self):
+        self.emitters = {}
+
+    def emit(self, ticket_emitter_dto: TicketEmitterDto) -> List[FakeTicket]:
+        tickets = []
+
+        for _ in range(ticket_emitter_dto.ticket_numbers):
+            ticket = FakeTicket(
+                ticket_emitter_dto.unary_price,
+                ticket_emitter_dto.event_name,
+                ticket_emitter_dto.event_date,
+                False,
+            )
+            tickets.append(ticket)
+
+        self.emitters["emitter"] = tickets
+        return tickets
+
+    def get_tickets(self, emitter) -> List[FakeTicket]:
+        return self.emitters[emitter]
