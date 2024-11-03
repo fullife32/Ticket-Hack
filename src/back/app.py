7fc77dd12bcfe2e6d2a557ceeee5f8b66c465f73
cardiox12 <com.bellavi@tutanota.com>
2024-11-03 16:56:27 +0100

    new version

--- src/back/app.py
+++ src/back/app.py
@@ -1,55 +1,58 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-from ticket_sdk.ticket import TicketEmitterDto, TicketsManager
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/tickets": {"origins": "*"}})
 
-app.config["APPLICATION_ROOT"] = "/api/v1"
-
-# Global state
-# Yes this is shit, it ain't much but it's honest work!
-tickets_manager = TicketsManager()
-
-@app.before_request
-def before_request():
-    headers = {
-        'Access-Control-Allow-Origin': '*', 
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', 
-        'Access-Control-Allow-Headers': 'Content-Type'
-    }
-    if request.method == 'OPTIONS' or request.method == 'options':
-        return jsonify(headers), 200
-
-@app.route("/tickets", methods=["POST", "OPTIONS"])
+@app.route("/tickets", methods=["POST"])
 def create_tickets():
-    print("GOT REQUEST")
-    try:
-        ticket_data = request.get_json()
-        ticket_dto = TicketEmitterDto(**ticket_data)
+    return jsonify({"message": "success"})
 
-        emitted_tickets = tickets_manager.emit(ticket_dto)
-
-        return jsonify({
-            "message": "success",
-            "tickets": [ticket.to_dict() for ticket in emitted_tickets],
-        })
-    except Exception as e:
-        return jsonify({
-            "message": "failure",
-            "error"  : str(e),
-        }), 400
-
-
-@app.route("/tickets", methods=["GET", "OPTIONS"])
+@app.route("/tickets", methods=["GET"])
 def get_tickets():
-    try:
-        return jsonify({
+    return jsonify(
+        {
             "message": "success",
-            "tickets": [ticket.to_dict() for ticket in tickets_manager.get_tickets("emitter")],
-        })
-    except Exception:
-        return jsonify({
-            "message": "failure",
-            "error": "no tickets has been created",
-        }), 400
+            "tickets": [
+                {
+                    "consumed": False,
+                    "event_date": "Tue, 05 Nov 2024 00:00:00 GMT",
+                    "event_name": "La fête du slip",
+                    "id": "0e5fff3f-c93e-4b1f-820c-eee101b73ad5",
+                    "price": 1.0
+                },
+                {
+                    "consumed": False,
+                    "event_date": "Tue, 05 Nov 2024 00:00:00 GMT",
+                    "event_name": "La fête du slip",
+                    "id": "dde695f0-cea2-429f-bb3f-fb5279affb4f",
+                    "price": 1.0
+                },
+                {
+                    "consumed": False,
+                    "event_date": "Tue, 05 Nov 2024 00:00:00 GMT",
+                    "event_name": "La fête du slip",
+                    "id": "2ef46d32-f3dc-4ee8-8bf7-34ae4c7d47a7",
+                    "price": 1.0
+                },
+                {
+                    "consumed": False,
+                    "event_date": "Tue, 05 Nov 2024 00:00:00 GMT",
+                    "event_name": "La fête du slip",
+                    "id": "5b8a10f3-4fcc-4aae-85ad-0105cbbc863d",
+                    "price": 1.0
+                },
+                {
+                    "consumed": False,
+                    "event_date": "Tue, 05 Nov 2024 00:00:00 GMT",
+                    "event_name": "La fête du slip",
+                    "id": "3942120e-584c-41af-a2be-9742fa7889ba",
+                    "price": 1.0
+                }
+            ]
+        }
+    )
+
+
+app.run(debug=True)
